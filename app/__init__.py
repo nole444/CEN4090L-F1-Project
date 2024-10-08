bdbc4662484f7572df58a8493be69ea803555c9a
nole444 <ep16g@fsu.edu>
2024-10-08 16:56:23 -0400

    Setup part of the app's architecture
    Added multiple directories for future html templates, configurations, and model classes. This is just the setup for the structure of the app.

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
