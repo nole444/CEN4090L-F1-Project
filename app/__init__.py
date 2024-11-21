c029e030360aa75b8fb294cf3b6fe670c8bbd71b
nole444 <157317526+nole444@users.noreply.github.com>
2024-11-21 18:54:40 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-from config import Config
+
 
 
 # Instantiate extensions
@@ -15,11 +15,13 @@ migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['SECRET_KEY'] = '5623'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:noles2021@localhost/F1'
 
-    # Initialize extensions
+    # Initialize extensions that we are utilizing
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
