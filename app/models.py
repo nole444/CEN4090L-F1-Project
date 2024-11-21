754371286d8d16fd39747139fba15ed4e6e164e1
nole444 <157317526+nole444@users.noreply.github.com>
2024-11-21 18:54:05 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # app/models.py
 
 from datetime import datetime
-from app import db, login_manager
+from app import db, login_manager, bcrypt
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -17,17 +17,15 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(130), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
     role = db.relationship('Role', backref='users')
 
-    # Set the password, storing the hashed version
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    # Check the password against the stored hash
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 # Role model for role-based access control
 class Role(db.Model):
