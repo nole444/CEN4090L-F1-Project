615042981c7d7952012763ff210633cd07ec5914
Keene <keenemeck@gmail.com>
2024-12-13 11:05:41 -0500

    document updates

--- Simv2.py
+++ Simv2.py
@@ -6,10 +6,12 @@ def display_strategy_comparison(selected_driver_name, laps, strategies, tire_typ
     results = []
     for strategy in strategies:
         strategy_copy = strategy.copy()
-        total_time, fastest_lap = simulate_race(selected_driver_name, strategy_copy, laps, tire_types, pit_stop_time)
+        total_time, fastest_lap = simulate_race(
+            selected_driver_name, strategy_copy, laps, tire_types, pit_stop_time)
         time_diff = total_time - selected_strategy_time
         time_diff_percentage = (time_diff / selected_strategy_time) * 100
-        strategy_desc = f"Start Tire: {strategy['start_tire']}, Pit Laps: {strategy['pit_laps']}, Onto: {strategy['next_tires']}"
+        strategy_desc = f"Start Tire: {strategy['start_tire']}, Pit Laps: {
+            strategy['pit_laps']}, Onto: {strategy['next_tires']}"
         results.append({
             "strategy": strategy_desc,
             "total_time": total_time,
@@ -27,12 +29,14 @@ def display_strategy_comparison(selected_driver_name, laps, strategies, tire_typ
 def display_timing_board(cars):
     timingBoard = []
     print("\n--- Timing Board ---")
-    print(f"{'Position':<10}{'Car':<10}{'Driver':<15}{'Pit Stops':<12}{'Fastest Lap':<15}{'Total Race Time':<20}")
+    print(f"{'Position':<10}{'Car':<10}{'Driver':<15}{
+          'Pit Stops':<12}{'Fastest Lap':<15}{'Total Race Time':<20}")
     cars.sort(key=lambda x: x['time'])
     for position, car in enumerate(cars, start=1):
         total_time_str = f"{car['time']:.3f}s"
         fastest_lap_str = f"{car['fastest_lap']:.3f}s"
-        print(f"{position:<10}{car['car_id']:<10}{car['driver_name']:<15}{car['pit_stops']:<12}{fastest_lap_str:<15}{total_time_str:<20}")
+        print(f"{position:<10}{car['car_id']:<10}{car['driver_name']:<15}{
+              car['pit_stops']:<12}{fastest_lap_str:<15}{total_time_str:<20}")
 
         timingBoard.append({
             "position": position,
@@ -44,6 +48,7 @@ def display_timing_board(cars):
         })
 
     return timingBoard
+
 
 def simulate_race(driver, strategy, laps, tire_types, pit_stop_time=22):
     current_tire = strategy['start_tire']
@@ -74,6 +79,7 @@ def simulate_race(driver, strategy, laps, tire_types, pit_stop_time=22):
 
     return total_time, fastest_lap
 
+
 def simEngine(APIdata, strat_select, grid):
     results = {}
 
@@ -88,30 +94,54 @@ def simEngine(APIdata, strat_select, grid):
     }
 
     track_dict = [
-        {"circuit_key": 63,"track_name": "Bahrain International Circuit", "track_length_miles": 3.363, "number_of_laps": 57},
-        {"circuit_key": 149,"track_name": "Jeddah Corniche Circuit", "track_length_miles": 3.836, "number_of_laps": 50},
-        {"circuit_key": 10,"track_name": "Albert Park Circuit", "track_length_miles": 3.295, "number_of_laps": 58},
-        {"circuit_key": 46,"track_name": "Suzuka International Racing Course", "track_length_miles": 3.609, "number_of_laps": 53},
-        {"circuit_key": 49,"track_name": "Shanghai International Circuit", "track_length_miles": 3.388, "number_of_laps": 56},
-        {"circuit_key": 151,"track_name": "Miami International Autodrome", "track_length_miles": 3.362, "number_of_laps": 57},
-        {"circuit_key": 6,"track_name": "Autodromo Enzo e Dino Ferrari (Imola)", "track_length_miles": 3.050, "number_of_laps": 63},
-        {"circuit_key": 22,"track_name": "Circuit de Monaco", "track_length_miles": 2.074, "number_of_laps": 78},
-        {"circuit_key": 23,"track_name": "Circuit Gilles Villeneuve", "track_length_miles": 2.710, "number_of_laps": 70},
-        {"circuit_key": 23,"track_name": "Circuit de Barcelona-Catalunya", "track_length_miles": 2.892, "number_of_laps": 66},
-        {"circuit_key": 19,"track_name": "Red Bull Ring", "track_length_miles": 2.683, "number_of_laps": 71},
-        {"circuit_key": 2,"track_name": "Silverstone Circuit", "track_length_miles": 3.661, "number_of_laps": 52},
-        {"circuit_key": 4,"track_name": "Hungaroring", "track_length_miles": 2.722, "number_of_laps": 70},
-        {"circuit_key": 7,"track_name": "Circuit de Spa-Francorchamps", "track_length_miles": 4.352, "number_of_laps": 44},
-        {"circuit_key": 55,"track_name": "Circuit Zandvoort", "track_length_miles": 2.647, "number_of_laps": 72},
-        {"circuit_key": 39,"track_name": "Autodromo Nazionale Monza", "track_length_miles": 3.600, "number_of_laps": 53},
-        {"circuit_key": 144,"track_name": "Baku City Circuit", "track_length_miles": 3.730, "number_of_laps": 51},
-        {"circuit_key": 61,"track_name": "Marina Bay Street Circuit", "track_length_miles": 3.146, "number_of_laps": 61},
-        {"circuit_key": 9,"track_name": "Circuit of the Americas", "track_length_miles": 3.426, "number_of_laps": 56},
-        {"circuit_key": 65,"track_name": "Autódromo Hermanos Rodríguez", "track_length_miles": 2.674, "number_of_laps": 71},
-        {"circuit_key": 14,"track_name": "Interlagos Circuit", "track_length_miles": 2.677, "number_of_laps": 71},
-        {"circuit_key": 152,"track_name": "Las Vegas Strip Circuit", "track_length_miles": 3.852, "number_of_laps": 50},
-        {"circuit_key": 150,"track_name": "Lusail International Circuit", "track_length_miles": 3.367, "number_of_laps": 57},
-        {"circuit_key": 70,"track_name": "Yas Marina Circuit", "track_length_miles": 3.281, "number_of_laps": 58}
+        {"circuit_key": 63, "track_name": "Bahrain International Circuit",
+            "track_length_miles": 3.363, "number_of_laps": 57},
+        {"circuit_key": 149, "track_name": "Jeddah Corniche Circuit",
+            "track_length_miles": 3.836, "number_of_laps": 50},
+        {"circuit_key": 10, "track_name": "Albert Park Circuit",
+            "track_length_miles": 3.295, "number_of_laps": 58},
+        {"circuit_key": 46, "track_name": "Suzuka International Racing Course",
+            "track_length_miles": 3.609, "number_of_laps": 53},
+        {"circuit_key": 49, "track_name": "Shanghai International Circuit",
+            "track_length_miles": 3.388, "number_of_laps": 56},
+        {"circuit_key": 151, "track_name": "Miami International Autodrome",
+            "track_length_miles": 3.362, "number_of_laps": 57},
+        {"circuit_key": 6,
+            "track_name": "Autodromo Enzo e Dino Ferrari (Imola)", "track_length_miles": 3.050, "number_of_laps": 63},
+        {"circuit_key": 22, "track_name": "Circuit de Monaco",
+            "track_length_miles": 2.074, "number_of_laps": 78},
+        {"circuit_key": 23, "track_name": "Circuit Gilles Villeneuve",
+            "track_length_miles": 2.710, "number_of_laps": 70},
+        {"circuit_key": 23, "track_name": "Circuit de Barcelona-Catalunya",
+            "track_length_miles": 2.892, "number_of_laps": 66},
+        {"circuit_key": 19, "track_name": "Red Bull Ring",
+            "track_length_miles": 2.683, "number_of_laps": 71},
+        {"circuit_key": 2, "track_name": "Silverstone Circuit",
+            "track_length_miles": 3.661, "number_of_laps": 52},
+        {"circuit_key": 4, "track_name": "Hungaroring",
+            "track_length_miles": 2.722, "number_of_laps": 70},
+        {"circuit_key": 7, "track_name": "Circuit de Spa-Francorchamps",
+            "track_length_miles": 4.352, "number_of_laps": 44},
+        {"circuit_key": 55, "track_name": "Circuit Zandvoort",
+            "track_length_miles": 2.647, "number_of_laps": 72},
+        {"circuit_key": 39, "track_name": "Autodromo Nazionale Monza",
+            "track_length_miles": 3.600, "number_of_laps": 53},
+        {"circuit_key": 144, "track_name": "Baku City Circuit",
+            "track_length_miles": 3.730, "number_of_laps": 51},
+        {"circuit_key": 61, "track_name": "Marina Bay Street Circuit",
+            "track_length_miles": 3.146, "number_of_laps": 61},
+        {"circuit_key": 9, "track_name": "Circuit of the Americas",
+            "track_length_miles": 3.426, "number_of_laps": 56},
+        {"circuit_key": 65, "track_name": "Autódromo Hermanos Rodríguez",
+            "track_length_miles": 2.674, "number_of_laps": 71},
+        {"circuit_key": 14, "track_name": "Interlagos Circuit",
+            "track_length_miles": 2.677, "number_of_laps": 71},
+        {"circuit_key": 152, "track_name": "Las Vegas Strip Circuit",
+            "track_length_miles": 3.852, "number_of_laps": 50},
+        {"circuit_key": 150, "track_name": "Lusail International Circuit",
+            "track_length_miles": 3.367, "number_of_laps": 57},
+        {"circuit_key": 70, "track_name": "Yas Marina Circuit",
+            "track_length_miles": 3.281, "number_of_laps": 58}
     ]
 
     tire_types = {
@@ -121,21 +151,27 @@ def simEngine(APIdata, strat_select, grid):
     }
 
     strategies = [
-        {'start_tire': 'soft', 'pit_laps': [20, 40], 'next_tires': ['medium', 'hard']},
+        {'start_tire': 'soft', 'pit_laps': [
+            20, 40], 'next_tires': ['medium', 'hard']},
         {'start_tire': 'soft', 'pit_laps': [20], 'next_tires': ['hard']},
-        {'start_tire': 'soft', 'pit_laps': [15, 45], 'next_tires': ['hard', 'hard',]},
+        {'start_tire': 'soft', 'pit_laps': [
+            15, 45], 'next_tires': ['hard', 'hard',]},
         {'start_tire': 'medium', 'pit_laps': [25], 'next_tires': ['hard']},
-        {'start_tire': 'medium', 'pit_laps': [20, 50], 'next_tires': ['hard', 'soft']},
-        {'start_tire': 'hard', 'pit_laps': [30, 47], 'next_tires': ['medium', 'medium']},
-        
+        {'start_tire': 'medium', 'pit_laps': [
+            20, 50], 'next_tires': ['hard', 'soft']},
+        {'start_tire': 'hard', 'pit_laps': [
+            30, 47], 'next_tires': ['medium', 'medium']},
+
     ]
 
     selected_driver_id = APIdata['driver_number']
     selected_driver_name = driver_dict[APIdata['driver_number']]
 
-    selected_track = next((track for track in track_dict if track["circuit_key"] == APIdata['circuit_key']), None)
+    selected_track = next(
+        (track for track in track_dict if track["circuit_key"] == APIdata['circuit_key']), None)
     if not selected_track:
-        raise ValueError(f"Track with circuit key {APIdata['circuit_key']} not found!")
+        raise ValueError(f"Track with circuit key {
+                         APIdata['circuit_key']} not found!")
     laps = selected_track['number_of_laps']
 
     print(f"You selected {selected_track['track_name']} ({laps} laps).\n")
@@ -145,7 +181,7 @@ def simEngine(APIdata, strat_select, grid):
 
     for position, car_id in enumerate(grid):
         driver_name = driver_dict[grid[position]]
-        #timing_offset = position * .5
+        # timing_offset = position * .5
         position += 1
         if car_id == selected_driver_id:
 
@@ -153,7 +189,8 @@ def simEngine(APIdata, strat_select, grid):
         else:
             strategy = random.choice(strategies)
 
-        total_time, fastest_lap = simulate_race(driver_name, strategy, laps, tire_types)
+        total_time, fastest_lap = simulate_race(
+            driver_name, strategy, laps, tire_types)
         car_data = {
             "car_id": car_id,
             "driver_name": driver_name,
@@ -166,8 +203,8 @@ def simEngine(APIdata, strat_select, grid):
             selected_driver_result = car_data
         cars.append(car_data)
 
-    #print(f"Debug: Selected Driver Result - {selected_driver_result}")
-    
+    # print(f"Debug: Selected Driver Result - {selected_driver_result}")
+
     results = display_timing_board(cars)
 
     simulated_strategies = {}
@@ -177,19 +214,20 @@ def simEngine(APIdata, strat_select, grid):
         strategy_key = str(strategy)
         if strategy_key not in simulated_strategies:
             # Simulate the race for this strategy if not already done
-            total_time, _ = simulate_race(selected_driver_result['driver_name'], strategy, laps, tire_types)
-            #print(f"{total_time}")
+            total_time, _ = simulate_race(
+                selected_driver_result['driver_name'], strategy, laps, tire_types)
+            # print(f"{total_time}")
             simulated_strategies[strategy_key] = total_time
         else:
             total_time = simulated_strategies[strategy_key]
-    
+
         optimal_time = min(optimal_time, total_time)
 
     # Calculate chosen strategy's accuracy
     chosen_time = selected_driver_result['time']
     accuracy = (optimal_time / chosen_time) * 100
 
-    return{
+    return {
         "timing_board": results,
         "strategy_accuracy": accuracy
     }
